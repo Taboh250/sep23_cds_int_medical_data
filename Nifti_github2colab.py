f44277dd19ece8e18be35fdde05c9865080a8c22
jricardoct15 <65625800+jricardoct15@users.noreply.github.com>
2024-01-18 16:53:35 +0100

    Add files via upload

---
+++ Nifti_github2colab.py
@@ -0,0 +1,15 @@
+import nibabel as nib
+
+def OpenNiftiGitHubFile (PathAndFileName,UserName= "jricardoct15",GithubRepo="sep23_cds_int_medical_data"):
+  ''' Open nifti files from git hub when executed from colab
+  User - github user name
+  PathAndFileName - includs path/filename.nii.gz
+  GithubRepo - Github Repository
+  '''
+  url = f"https://github.com/{UserName}/{GithubRepo}/raw/main/{PathAndFileName}"
+  !wget --no-cache --backups=1 {url}
+
+  data = nib.load(PathAndFileName).get_fdata()
+
+  !rm {PathAndFileName}
+  return data
